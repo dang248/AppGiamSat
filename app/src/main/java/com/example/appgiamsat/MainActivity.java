73007f885f99439c4061d40b564878fde6295b15
dang248 <lequocdangbku18@gmail.com>
2022-04-06 09:06:51 +0700

    LuanVanCuoiKhoa

---
+++ app/src/main/java/com/example/appgiamsat/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.appgiamsat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+     Button btnLoginMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLoginMain = (Button) findViewById(R.id.btnLogin);
+        btnLoginMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PieChartActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/appgiamsat/PieChartActivity.java
@@ -0,0 +1,75 @@
+package com.example.appgiamsat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class PieChartActivity extends AppCompatActivity {
+    private PieChart pieChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pie_chart);
+        pieChart = findViewById(R.id.pieChartActivity);
+        setupPieChart();
+        loadPieChartData();
+
+    }
+    private void setupPieChart(){
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterText("State Machine");
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment((Legend.LegendHorizontalAlignment.RIGHT));
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+
+    }
+    private void loadPieChartData(){
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.2f, "Waiting"));
+        entries.add(new PieEntry(0.15f, "Running"));
+        entries.add(new PieEntry(0.1f, "Prepairing"));
+        entries.add(new PieEntry(0.3f, "Off"));
+        entries.add(new PieEntry(0.25f, "Ready"));
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+        for (int color: ColorTemplate.MATERIAL_COLORS){
+            colors.add(color);
+        }
+
+        for (int color: ColorTemplate.VORDIPLOM_COLORS){
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+    }
+}
